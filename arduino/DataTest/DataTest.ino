25659296a2d92f19b0fdda0ccd8a39c688d2a4fb
chrisclai <chrisclai02@gmail.com>
2021-02-07 23:11:24 -0800

    Arduino DataRead File Added
    Added Arduino file that spits out random numbers corresponding to each of the sensors that will eventually be connected to it. Swapped around a few files in the Socket folder as well

---
+++ arduino/DataTest/DataTest.ino
@@ -0,0 +1,101 @@
+// Project: DataTest
+// Author: Christopher Lai
+// Description: Prints out random numbers signifying each sensor to the serial monitor.
+// Please refer to the CPP Hyperloop spreadsheet for specific array IDs.
+// Version Control: 2.7.2021
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define tempSensor 6
+#define testLED 13
+
+OneWire oneWire (tempSensor);
+
+DallasTemperature sensors(&oneWire);
+
+void RPiSerial()  // Checks to see if serial data has been recieved from the Raspberry Pi
+{
+  if (Serial.available())
+  {
+    String command = Serial.readString();
+    if (command == "start")
+    {
+      digitalWrite(testLED, HIGH);
+    }
+    else if (command == "stop")
+    {
+      digitalWrite(testLED, LOW);
+    }
+  }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  pinMode(testLED, OUTPUT);   // Use the on-board LED on the Arduino to test input from GUI
+  digitalWrite(testLED, LOW); // On-start, LED is OFF by default
+
+  randomSeed(analogRead(0));  // Generate Random Numbers
+  
+  // sensors.begin();
+  // delay(1000);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  // Unused code for this example, normally would be for temperature data
+  // sensors.requestTemperatures();
+  // double tempF = sensors.getTempFByIndex(0);
+  // Serial.println(String(tempF) + "F");
+
+  int sensorData[27];
+
+  // Temperature Sensor Readings [5]
+  sensorData[0] = random(20, 40); // Temp Sensor Motor Controller 1
+  sensorData[1] = random(20, 40); // Temp Sensor Motor Controller 2
+  sensorData[2] = random(20, 40); // Temp Sensor Motor 1
+  sensorData[3] = random(20, 40); // Temp Sensor Motor 2
+  sensorData[4] = random(20, 40); // Temp Sensor Battery System
+
+  // IMU Readings [16]
+  sensorData[5] = random(100);      // Absolute Orientation x-component
+  sensorData[6] = random(100);      // Absolute Orientation y-component
+  sensorData[7] = random(100);      // Absolute Orientation z-component
+  sensorData[8] = random(100);      // Angular Velocity Vector x-component
+  sensorData[9] = random(100);      // Angular Velocity Vector y-component
+  sensorData[10] = random(100);     // Angular Velocity Vector z-component
+  sensorData[11] = random(100);     // Acceleration Vector x-component
+  sensorData[12] = random(100);     // Acceleration Vector y-component
+  sensorData[13] = random(100);     // Acceleration Vector z-component
+  sensorData[14] = random(100);     // Linear Acceleration Vector x-component
+  sensorData[15] = random(100);     // Linear Acceleration Vector y-component
+  sensorData[16] = random(100);     // Linear Acceleration Vector z-component
+  sensorData[17] = random(100);     // Gravity Vector x-component
+  sensorData[18] = random(100);     // Gravity Vector y-component
+  sensorData[19] = random(100);     // Gravity Vector z-component
+  sensorData[20] = random(20, 40);  // Ambient Temperature IMU
+
+  // Pressure Sensor [1]
+  sensorData[21] = random(50, 150); // Pressure in kPa
+
+  // Current + Voltage Sensor [5]
+  sensorData[22] = random(30, 70);   // Motor Voltage (V)
+  sensorData[23] = random(50, 100);  // Motor Current (mA)
+  sensorData[24] = random(30, 70);   // Battery Voltage (V)
+  sensorData[25] = random(50, 100);  // Battery Current (mA)
+  sensorData[26] = random(90, 100);  // Battery Capacity (%)
+
+  String printString = "";
+  for (int i = 0; i < 27; i++)
+  {
+    printString += String(sensorData[i]) + " ";
+  }
+  Serial.println(printString);
+  
+  RPiSerial();
+
+  delay(100);
+}
