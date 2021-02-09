169071f725abe136a7ad2c161b1e3bb13206916b
chrisclai <chrisclai02@gmail.com>
2021-02-09 01:14:03 -0800

    updated socket code

--- socket/clientTest.py
+++ socket/clientTest.py
@@ -1,7 +1,8 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("192.168.68.123", 1234))
+s.connect(("192.168.68.125", 1234))
 
-message = s.recv(1024)
-print(message.decode('utf-8'))
+while(True):    
+    message = s.recv(1024)
+    print(message.decode('utf-8'))
