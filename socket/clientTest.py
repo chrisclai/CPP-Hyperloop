0e825055f4f3333f732ec894af9ec2e373f4d3ee
24alexwolf <56744627+24alexwolf@users.noreply.github.com>
2021-02-19 13:31:06 -0800

    Help - Alex GUI
    Sorry it made me commit

--- socket/clientTest.py
+++ socket/clientTest.py
@@ -1,7 +1,7 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("192.168.86.38", 1234))
+s.connect(("192.168.1.19", 1234))
 
 while(True):    
     message = s.recv(1024)
