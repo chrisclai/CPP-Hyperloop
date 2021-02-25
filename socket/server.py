13de16932e44e8cf4f90132c2bc41ddec5a86a9a
chrisclai <chrisclai02@gmail.com>
2021-02-25 10:36:49 +0000

    modified arduino-rpi-server information

--- socket/server.py
+++ socket/server.py
@@ -2,23 +2,22 @@ import serial
 import socket
 import time
 
-ser = serial.Serial("/dev/ttyACM0", 9600)
+ser = serial.Serial("/dev/ttyACM0", 115200)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind(("192.168.68.125", 1234))
+s.bind(("192.168.68.120", 1234))
 s.listen(5)
-time.sleep(2)
-data = ser.readline()
-time.sleep(0)
-data = 0
 
 while True:
     clientsocket, address = s.accept()
     print(f"Connection from {address} has been established!")
     while True:
         data = ser.readline()
-        decoded = data.decode('utf-8').strip()
-        print(decoded)
-        clientsocket.send(bytes(decoded, 'utf-8'))
-        time.sleep(0.1)
+        try:
+                decoded = data.decode('utf-8')
+                print(decoded)
+                clientsocket.send(bytes(decoded, 'utf-8'))
+                time.sleep(0.1)
+        except UnicodeDecodeError:
+                print("unicode error detected... anyways,")
