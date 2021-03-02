bf9c9000f6cae7db6e478a012dd53d685943ed1c
chrisclai <chrisclai02@gmail.com>
2021-03-02 03:13:23 -0800

    Update clientTest.py

--- socket/clients/clientTest.py
+++ socket/clients/clientTest.py
@@ -1,10 +1,20 @@
+import sys
 import socket
+import time
 
-# this file is a test
+def Main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 clientThread.py <hostID>")
+        sys.exit(1)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("192.168.68.120", 1234))
+    serverIP = sys.argv[1]
+    
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((serverIP, 1234))
 
-while(True):    
-    message = s.recv(1024)
-    print(message.decode('utf-8'))
+    while(True):    
+        message = s.send(bytes("hi", 'utf-8'))
+        time.sleep(0.25)
+
+if __name__ == '__main__': 
+    Main() 
