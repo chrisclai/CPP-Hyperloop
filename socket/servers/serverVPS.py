a93ac1eb31ce899da2e79fd5c04f196844c39f22
chrisclai <chrisclai02@gmail.com>
2021-03-02 03:56:05 -0800

    updated serverVPS logic

--- socket/servers/serverVPS.py
+++ socket/servers/serverVPS.py
@@ -6,13 +6,27 @@ from _thread import *
 import threading  
 
 # thread function 
-def handle_client(conn, addr, ser): 
-    #print(f"[NEW CONNECTION] {addr} connected.")
-    
+def read_write_async(connIn, connOut, addr): 
+    print(f"[NEW CONNECTION] {addr} connected to r-w-a thread.")
     while True:
         msg = conn.recv(16).decode('utf-8')
         print(msg)
-        ser.write(msg.encode('utf-8'))
+        connOut.send(bytes(msg, 'utf-8'))
+
+def read_write_sync(connIn, connOut, addr)
+    print(f"[NEW CONNECTION] {addr} connected to r-w-s thread.")
+    while True:
+        msg = connIn.recv(4096)
+        if not msg:
+            connIn.close()
+            print("Process terminated. Closing socket.")
+            break
+        else:
+            try:
+                connOut.send(msg)
+            except:
+                print(f"Packet send attempt to {addr} failed. Closing connection.")
+                connOut.close()
 
 def Main(): 
     if len(sys.argv) != 1:
@@ -30,22 +44,31 @@ def Main():
     print("socket binded to port", port) 
 
     # put the socket into listening mode 
-    s.listen(5) 
+    s.listen(5)
+    s.settimeout(5) 
     print("socket is listening") 
 
-    clientsocket, address = s.accept()
-    print(f"Connection from {address} has been established!")
+    # dictionary of open sockets
+    sockdict = {}
 
-    # a forever loop until client wants to exit 
-    while True:  
-        msg = clientsocket.recv(4096).decode('utf-8')
-        if not msg:
-            print("Socket Closed.")
-            s.close()
-            break
-        else:
-            print(msg)
-    print("Program Terminated.") 
+    try:
+        # create a specialized socket just for the RPi on boot
+        rpiclientsocket, rpiaddress = s.accept()
+        sockdict[rpiclientsocket] = rpiaddress
+        print(f"Connection from {rpiaddress} has been established! [THIS IS THE RASPBERRY PI]")
+
+        while True:
+            try:
+                conn, addr = s.accept()       
+                print(f"Connection from {addr} has been established!")
+                sockdict[conn] = addr
+                print(f"Socket dictionary: {sockdict}")
+                thread = threading.Thread(target = read_write_sync, args=(rpiclientsocket, conn, addr)
+                thread.start()
+            except:
+                pass   
+    except:
+        print("RPi not found. Please run program with connected device.")            
 
 if __name__ == '__main__': 
     Main() 
