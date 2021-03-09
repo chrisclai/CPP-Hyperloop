426aff9dc011c806e0bbf23e76f2fcde38e0e9e2
chrisclai <chrisclai02@gmail.com>
2021-03-08 21:22:58 -0800

    updated raspberry pi logic with all the data list elements

--- socket/clients/clientRPi.py
+++ socket/clients/clientRPi.py
@@ -6,18 +6,67 @@ import socket
 from _thread import *
 import threading
 
-def read_write_async(connIn, connOut, addr): 
-    print(f"[NEW CONNECTION] {addr} connected to r-w-a thread")
+global templist
+templist = [0, 0, 0, 0, 0]
+
+global controllist
+controllist = [0, 0]
+
+def control(connIn, connOut, addr): 
+    print(f"[CONTROL] {addr} Successfully connected to control thread!")
+    global controllist
     while True:
         try:
-            msg = connIn.recv(16)
-            connOut.write(msg)
-            print(msg.decode('utf-8'))
+            msg = connIn.recv(16).decode('utf-8')
+            print(msg)
+            connOut.write(msg.encode('utf-8'))
+            if msg == "brakeoff":
+                controllist[0] = 0
+                pass
+            elif msg == "brakeon":
+                controllist[0] = 1
+                pass
+            elif msg == "motoroff":
+                controllist[1] = 0
+                pass
+            elif msg == "motoron":
+                controllist[1] = 1
+                pass
         except: 
-            print(f"Packet write attempt to {addr} failed. Closing connection.")
-            break
+            print("Could not send command into arduino!!!")
         # connIn = server ; connOut = arduino - doesn't have addr; it is serial 
-        # addr is from server 
+        # addr is from server
+
+def maindata(connIn, connOut, addr): 
+    print(f"[MAIN] {addr} Successfully connected to main thread!")
+    while True:
+        # how rasp pi receiving data from arduino \\ ser.writeline()
+        try:
+            data = connIn.readline().decode('utf-8').split()
+            print(data)
+        except UnicodeDecodeError:
+            print("unicode error detected... anyways,")
+        # update main data list with information from the temperature sensor readings and control readings
+        for x in range(0, 5):
+            data[x] = tempdata[x]
+        try:
+            data[34] = controllist[0]
+            data[35] = controllist[1]
+        except:
+            print("Control list segment at fault. Trying again...")
+        # create string again with all of the list elements and send them out to the GUI
+        dataSend = ""
+        for x in range data:
+            dataSend += x  + " "
+        connOut.send(bytes(dataSend, 'utf-8'))
+        
+def tempdata(connIn, connOut, addr): 
+    print(f"[TEMP] {addr} Successfully connected to tempdata thread!")
+    global templist
+    while True:
+        tempdata = connIn.readline().decode('utf-8').split()
+        for x in range(0, 5):
+            templist[x] = tempdata[x]
 
 def Main():
     if len(sys.argv) != 2:
@@ -26,30 +75,26 @@ def Main():
     
     serverIP = sys.argv[1]
 
-    ser = serial.Serial("/dev/ttyACM0", 115200) # can write and read from it 
-    # ser = serial.Serial("/dev/ttyACM1", 115200)
-    nano = serial.Serial("/dev/ttyUSB0", 115200)
+    serMega = serial.Serial("/dev/ttyACM0", 115200) # can write and read from it 
+    serUno = serial.Serial("/dev/ttyACM1", 115200)
+    serNano = serial.Serial("/dev/ttyUSB0", 115200)
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((serverIP, 1234))
 
     print(f"Connection from {serverIP} has been established!")
-    thread_async = threading.Thread(target = read_write_async, args=(s, nano, serverIP))
+
+    # [THREAD] Control Thread to recieve inputs from the GUI
+    thread_async = threading.Thread(target = control, args=(s, serNano, serverIP))
     thread_async.start()
-    # rasp pi wants to receive data asynchronously from server 
     
-    time.sleep(3)
-    
-    while True:
-        data = ser.readline() # how rasp pi receiving data from arduino \\ ser.writeline()
-        # tempdata = tempard.readline()
-        
-        try:
-                print(data.decode('utf-8'))
-                # print(tempdata.decode('utf-8'))
-                s.send(data) # send bytes over to socket (server)
-        except UnicodeDecodeError:
-                print("unicode error detected... anyways,")
+    # [THREAD] Recieves IMU data from the MEGA, organizes data from other threads, and sends it off to the GUI
+    thread_main = threading.Thread(target = maindata, args=(serMega, s, serverIP))
+    thread_main.start()
+
+    # [THREAD] Off-Sync thread to recieve temperature data from UNO
+    thread_temp = threading.Thread(target = tempdata, args=(serUno, s, serverIP))
+    thread_temp.start()
 
 
 if __name__ == '__main__': 
