adc4afcc098a2bbc448121d3c119ae421add2cbf
chrisclai <chrisclai02@gmail.com>
2021-06-16 18:52:38 -0700

    dumb solution to a merge conflict

--- socket/clientTest.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("192.168.68.120", 1234))
-
-while(True):    
-    message = s.recv(1024)
-    print(message.decode('utf-8'))
