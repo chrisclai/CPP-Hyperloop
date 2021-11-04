2524370daf2baba22b768adf4ea879a179f6801c
TimmyTimTim30 <82090527+TimmyTimTim30@users.noreply.github.com>
2021-11-03 18:11:42 -0700

    Update Rpito2arduino.py

--- Official Release/Rpito2arduino.py
+++ Official Release/Rpito2arduino.py
@@ -2,16 +2,16 @@ import serial
 import random
 import time
 if __name__ == '__main__':
-    ser1 = serial.Serial("/dev/ttyUSB0", 115200)
-    ser2 = serial.Serial("/dev/ttyUSB1", 115200)
+    ser1 = serial.Serial("/dev/ttyUSB0", 115200) # reads serial from USB port 0
+    ser2 = serial.Serial("/dev/ttyUSB1", 115200) # reads serial from USB port 1
     ser1.flush()
     ser2.flush()
     while True:
-        time.sleep(1)
-        letter = ser1.readline().decode('utf-8').rstrip()
+        time.sleep(1) # allows for a delay so it doesn't get clogged up and confused
+        letter = ser1.readline().decode('utf-8').rstrip() # reads the serial that it got from the arduino transmitter and decodes it
         if letter == "x":
             print(letter)
-            ser2.write(letter.encode('utf-8'))
+            ser2.write(letter.encode('utf-8')) # sends out serial encoded in C++
         else:
             print(letter)
-            ser2.write(letter.encode('utf-8'))
+            ser2.write(letter.encode('utf-8')) # sends out serial encoded in C++
